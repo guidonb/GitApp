3edb8fe356486a5d0196811df634536178c816f7
גדעון ברנשטיין <guidon@h-macbook-pro-sl-gdwn.local>
2018-01-18 14:06:40 +0200

    Initial Commit

---
+++ GitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitApp
+//
+//  Created by גדעון ברנשטיין on 18.1.2018.
+//  Copyright © 2018 גדעון ברנשטיין. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
